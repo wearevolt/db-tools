8d2929afc29d9f6437e936a0e4d8e5d5c4a7ca69
Victor Zagorski <victor@zagorski.ru>
2020-02-29 19:57:56 +0700

    Added bash completion for psqlx

---
+++ bash_completion/psqlx_completion.bash
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Load autocompletion rules for pass
+if [ -f /usr/share/bash-completion/completions/pass ]; then
+  . /usr/share/bash-completion/completions/pass
+fi
+
+# Load autocompletion rules for psql
+if [ -f /usr/share/bash-completion/completions/psql ]; then
+  . /usr/share/bash-completion/completions/psql
+fi
+
+__is_function() {
+  [[ "$(declare -Ff "$1")" ]];
+}
+
+_psqlx() {
+  COMPREPLY=();
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+	local commands="--pass"
+
+  if [[ $COMP_CWORD -gt 1 ]]; then
+    local lastarg="${COMP_WORDS[$COMP_CWORD-1]}";
+
+    case $lastarg in
+      --pass)
+        # Use completion for pass show
+        __is_function _pass_complete_entries && _pass_complete_entries 1;
+      ;;
+
+      *)
+      # Use completion for psql
+        __is_function _psql && _psql psql;
+      ;;
+    esac;
+  else
+    # Use completion for psql
+    __is_function _psql && _psql psql;
+
+    # Add psqlx options
+    COMPREPLY+=( $(compgen -W "${commands}" -- ${cur}) );
+  fi;
+}
+
+complete -F _psqlx psqlx
